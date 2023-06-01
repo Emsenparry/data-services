fe9c4dfede33fdefba964240e975af8be2f2785d
Emma Nielsen <Emmanielsen843@gmail.com>
2023-06-01 09:07:04 +0200

    comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { router as ProductRouter } from "./Routes/Products.js";
+import { router as ProductRouter } from "./Routes/ProductRouter.js";
 
 const app = express()
 
+// Links kommer fra ProductRouter filen
 app.use(ProductRouter)
 
 
@@ -11,7 +12,7 @@ app.use((req, res) => {
     res.status(404).send("Siden blev ikke fundet")
 })
 
-
+// Hvilekn port lytter vi til
 app.listen(4242, () => {
     console.log('Server kører på port 4242: http://localhost:4242');
 })
--- index_http.js
+++ index_http.js
@@ -4,6 +4,7 @@ http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hej verden');
     res.end()
+    //Hvis den rammer den port skal den returnere en annonym funktion hvor der bliver console.log-gede.
 }).listen(4000, () => {
     console.log('Node server kører på http://localhost:4000');
 })
