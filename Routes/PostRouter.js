a6e100ff252ad5505878ac3ac89dcac0d61b880e
Emma Nielsen <Emmanielsen843@gmail.com>
2023-06-02 08:42:34 +0200

    comments

--- Routes/PostRouter.js
+++ Routes/PostRouter.js
@@ -1,13 +1,19 @@
+// Importerer dependencies
 import express from'express'
 
+// Deklarerer var til router
 const postRouter = express.Router()
 
-
+// Route med GET method - henter
 postRouter.get("/", (req, res) => {
+    // Get parametre hentes på request objektets query property
     console.log(req.query);
     res.send("Here you can get all posters");
 })
+// Route med POST method - opretter
 postRouter.post("/", (req, res) => {
+    // Henter form body data på res objektet og "sender" (udskriver) til browser
+	// Du kan logge req.body og se alle forespørgelsens form data 
     res.send(`${req.body.firstname} ${req.body.lastname}
     ${req.body.email} 
     ${req.body.birthday}
@@ -16,9 +22,13 @@ postRouter.post("/", (req, res) => {
     ${req.body.address}
     `);
 })
+
+// Route med PUT method - opdaterer
 postRouter.put("/", (req, res) => {
     res.send("Here you can update a poster");
 })
+
+// Route med DELETE method - Sletter
 postRouter.delete("/", (req, res) => {
     res.send("Here you can delete posters");
 })
--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import { postRouter } from './Routes/PostRouter.js';
 import { productRouter } from './Routes/ProductPoster.js';
+
+// Importerer og sætter dotenv til globale vars
+import dotenv from 'dotenv'
 dotenv.config()
 
-
+// Deklarerer app var med ekspress objekt
 const app = express()
+// Udvider app så vi kan læse form body data
 app.use(express.urlencoded({ extended: true}))
+// Deklarer port for at kunne bruge den længere nede
 const port = process.env.PORT;
 
+// Kalder root route - forsiden af vores app 
 app.get("/", (req, res) => {
     res.send("Velkommen til min NodeJS app. Igen..");
 })
 
-// Links kommer fra ProductRouter filen
+
+// Anvender eksterne routes
 app.use("/posts", postRouter)
 app.use(productRouter)
 
 
 
 
-//Hvis siden ikke kan findes
+// Skriver fejl hvis route ikke findes
 app.use((req, res) => {
     res.status(404).send("Siden blev ikke fundet")
 })
 
-// Hvilekn port lytter vi til
+// Aktiverer server og lytter på port fra .env fil
 app.listen(port, () => {
 console.log(`Server kører på http://localhost:${port}`);
 })
